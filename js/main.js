7c658773e1831bbc813355db7b14bfac5426d59a
VitasOnLine WORK <vitasonline@mail.ru>
2019-08-15 16:55:04 +0300

    4

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ $(document).ready(function () {
         items: 1,
         margin: 10,
         dots: 1,
-        nav: 0
+        nav: 0,
+        loop: 1,
+        autoplay: 0,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: 1
+    });
 
-    })
+    $('.header__button-menu').on('click', function () {
+        $('.menu').slideToggle();
+    });
+
+    $('.menu__item').on('click', function () {
+        $('.menu').slideToggle();
+    });
 });
